d04368b2a4bbe9bcb5f1ed28eae114b084e4b3de
Ildar5000 <ShamilovIldar@mail.ru>
2020-10-28 15:57:05 +0500

    AddSettings
    add SettingsModbus

--- SettingModbusForm.xaml.cs
+++ SettingModbusForm.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ModbusSyncStructLIb.Settings;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace ModbusSynchFormTest
 {
@@ -40,6 +43,56 @@ namespace ModbusSynchFormTest
 
             ListTypePartyComboBox.ItemsSource = typeParitylist;
             ListTypeStopbitsComboBox.ItemsSource = typeStopBitslist;
+
+            loadSettings();
+
+        }
+
+        private void loadSettings()
+        {
+            var path = System.IO.Path.GetFullPath(@"Settingsmodbus.xml");
+
+            if (File.Exists(path)==true)
+            {
+                SettingsModbus settings;
+                // десериализация
+                using (FileStream fs = new FileStream("Settingsmodbus.xml", FileMode.OpenOrCreate))
+                {
+                    XmlSerializer formatter = new XmlSerializer(typeof(SettingsModbus));
+                    settings = (SettingsModbus)formatter.Deserialize(fs);
+
+                    Console.WriteLine("Объект десериализован");
+                }
+                Com_name_txb.Text = settings.ComName;
+                BaudRate_txb.Text = settings.BoudRate.ToString();
+                DataBits_lb_txb.Text = settings.DataBits.ToString();
+                ReadTimeout_txt.Text = settings.ReadTimeout.ToString();
+                WriteTimeout_txt.Text = settings.WriteTimeout.ToString();
+                
+                foreach (var pt in typeParitylist)
+                {
+                    if (pt==settings.Party_type_str)
+                    {
+                        int index = typeParitylist.IndexOf(pt);
+                        ListTypePartyComboBox.SelectedIndex = index;
+                    }
+                }
+
+                foreach (var pt in typeStopBitslist)
+                {
+                    if (pt == settings.StopBits_type_str)
+                    {
+                        int index = typeStopBitslist.IndexOf(pt);
+                        ListTypeStopbitsComboBox.SelectedIndex = index;
+                    }
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Создайте файл ");
+            }
         }
 
         /// <summary>
@@ -49,6 +102,22 @@ namespace ModbusSynchFormTest
         /// <param name="e"></param>
         private void Apply_btn_Click(object sender, RoutedEventArgs e)
         {
+
+            
+            string selectedItem = (string)ListTypePartyComboBox.SelectedItem;
+
+            string selectedItem2 = (string)ListTypeStopbitsComboBox.SelectedItem;
+
+            SettingsModbus settings = new SettingsModbus(Com_name_txb.Text,Convert.ToInt32(BaudRate_txb.Text),Convert.ToInt32(DataBits_lb_txb.Text), selectedItem, selectedItem2, Convert.ToInt32(ReadTimeout_txt.Text),Convert.ToInt32(WriteTimeout_txt.Text));
+            XmlSerializer formatter = new XmlSerializer(typeof(SettingsModbus));
+
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("Settingsmodbus.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, settings);
+
+                Console.WriteLine("Объект сериализован");
+            }
 
         }
     }
